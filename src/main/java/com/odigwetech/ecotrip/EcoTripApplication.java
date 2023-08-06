f6e2002d69e05acdbc5da0350c1b68d3d9c211c6
Chukuwinke <rittnerodigwe@gmail.com>
2023-08-07 00:10:27 +0200

    initial commit

---
+++ src/main/java/com/odigwetech/ecotrip/EcoTripApplication.java
@@ -0,0 +1,13 @@
+package com.odigwetech.ecotrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoTripApplication.class, args);
+	}
+
+}
